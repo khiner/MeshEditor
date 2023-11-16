0198f938369b524761429ca0306720f0f6a62cb4
Karl Hiner <karl.hiner@gmail.com>
2023-11-16 12:41:07 -0500

    Delete unused `Vertex2D`

--- src/Vertex.h
+++ src/Vertex.h
@@ -1,13 +1,7 @@
 #pragma once
 
-#include <glm/vec2.hpp>
 #include <glm/vec3.hpp>
 #include <glm/vec4.hpp>
-
-struct Vertex2D {
-    glm::vec2 Position;
-    glm::vec4 Color;
-};
 
 struct Vertex3D {
     glm::vec3 Position;
