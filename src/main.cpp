5490cc514fc6b734179e97c517f01d5f8fbbc6a4
Karl Hiner <karl.hiner@gmail.com>
2025-01-12 14:29:22 -0800

    Looks like we no longer need `imgui_internal` header!

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 
 #include "imgui_impl_sdl3.h"
 #include "imgui_impl_vulkan.h"
-#include "imgui_internal.h"
+#include "imgui.h"
 #include "implot.h"
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_vulkan.h>
